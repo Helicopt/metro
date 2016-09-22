2699ce24382e848432759c51a1866989bdcfe035
WeitaoFeng <fwtt20071028@126.com>
2016-09-22 21:24:05 +0800

    recover2

--- Metro/mainView.h
+++ Metro/mainView.h
@@ -50,6 +50,9 @@ namespace Metro {
 	private: System::Windows::Forms::ListBox^  listBox1;
 
 	private: subway::subway * sd;
+	private: subway::planMode pm;
+	private: System::Windows::Forms::RadioButton^  radioButton1;
+	private: System::Windows::Forms::RadioButton^  radioButton2;
 
 	protected:
 
@@ -66,13 +69,14 @@ namespace Metro {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->radioButton1 = (gcnew System::Windows::Forms::RadioButton());
+			this->radioButton2 = (gcnew System::Windows::Forms::RadioButton());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -126,11 +130,37 @@ namespace Metro {
 			this->listBox1->Size = System::Drawing::Size(318, 316);
 			this->listBox1->TabIndex = 5;
 			// 
+			// radioButton1
+			// 
+			this->radioButton1->AutoSize = true;
+			this->radioButton1->Location = System::Drawing::Point(44, 97);
+			this->radioButton1->Name = L"radioButton1";
+			this->radioButton1->Size = System::Drawing::Size(59, 16);
+			this->radioButton1->TabIndex = 6;
+			this->radioButton1->TabStop = true;
+			this->radioButton1->Text = L"少换乘";
+			this->radioButton1->UseVisualStyleBackColor = true;
+			this->radioButton1->CheckedChanged += gcnew System::EventHandler(this, &mainView::radioButton1_CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this->radioButton2->AutoSize = true;
+			this->radioButton2->Location = System::Drawing::Point(134, 97);
+			this->radioButton2->Name = L"radioButton2";
+			this->radioButton2->Size = System::Drawing::Size(59, 16);
+			this->radioButton2->TabIndex = 7;
+			this->radioButton2->TabStop = true;
+			this->radioButton2->Text = L"少停站";
+			this->radioButton2->UseVisualStyleBackColor = true;
+			this->radioButton2->CheckedChanged += gcnew System::EventHandler(this, &mainView::radioButton2_CheckedChanged);
+			// 
 			// mainView
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(626, 365);
+			this->Controls->Add(this->radioButton2);
+			this->Controls->Add(this->radioButton1);
 			this->Controls->Add(this->listBox1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox2);
@@ -156,16 +186,21 @@ namespace Metro {
 		this->sd = new subway::subway(std::string("./beijing-subway.txt"));
 		this->textBox1->Text = "知春路";
 		this->textBox2->Text = "沙河";
+		this->pm = subway::pm_Short;
+		this->radioButton2->Checked = true;
 	}
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 		System::String^ st1 = this->textBox1->Text;
 		System::String^ st2 = this->textBox2->Text;
-		std::string *s=this->stdString(st1);
+		std::string *s = this->stdString(st1);
 		std::string *t = this->stdString(st2);
 		this->listBox1->Items->Clear();
 		this->button1->Enabled = false;
-		if (this->sd->do_c(*s, *t) == 0) {
-			this->listBox1->Items->Add("站数："+Convert::ToString(this->sd->getPlanNums().first)+"， 换乘次数："+Convert::ToString(this->sd->getPlanNums().second));
+		int flag;
+		if (this->pm == subway::pm_convi) flag = this->sd->do_c(*s, *t);
+		else if (this->pm == subway::pm_Short) flag = this->sd->do_b(*s, *t);
+		if (flag == 0) {
+			this->listBox1->Items->Add("站数：" + Convert::ToString(this->sd->getPlanNums().first) + "， 换乘次数：" + Convert::ToString(this->sd->getPlanNums().second));
 			for (int i = 0; i < this->sd->getCount(); ++i) {
 				this->listBox1->Items->Add(this->SysString(this->sd->getStep(i).c_str()));
 			}
@@ -174,6 +209,12 @@ namespace Metro {
 			this->listBox1->Items->Add("找不到站点");
 		}
 		this->button1->Enabled = true;
+	}
+	private: System::Void radioButton1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		this->pm = subway::pm_convi;
+	}
+	private: System::Void radioButton2_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		this->pm = subway::pm_Short;
 	}
 	};
 }
